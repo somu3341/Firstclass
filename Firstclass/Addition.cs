d69a59a16d6df07344a8cb1131ef0ca5ec9633ed
somu3341 <somashekarvsv1996@gmail.com>
2023-03-02 20:29:57 +0530

    [somu]added newfiles

---
+++ Firstclass/Addition.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Firstclass
+{
+    internal class Addition
+    {
+        static int a = 10;
+        static int b = 20;
+        static int c = (a+b);
+        static void Main(string[] args)
+        {            
+            Console.WriteLine("addition of a and b is " +c);
+        }
+  
+    }
+}
